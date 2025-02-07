4b51e513c1d11c0c19f93e5f3ead39ccc3bb75bf
SebaVaccaro <soy.vaccaro@gmail.com>
2025-02-07 13:31:27 -0300

    implement cors

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://e-commerce-woad-phi-43.vercel.app'],
+    origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
   
   const PORT = process.env.PORT || 3000;
-  app.use(cookieParser());
 
   await app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 }
